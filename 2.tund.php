098a5afcc500fb3e7ac4925f5f5004e8aedab8c3
taankomm <taankomm@greeny.cs.tlu.ee>
2016-09-14 14:48:56 +0300

    added if and for loop

---
+++ 2.tund.php
@@ -0,0 +1,35 @@
+<?php
+
+	$age = 5;
+	
+	if ($age < 18) {
+		
+		echo "alaealine";
+		
+	} else {
+		
+		echo "täisealine";
+		
+	}
+
+?>
+
+<br>
+
+<?php
+	
+	//for(1;2;3) {sisu}
+	// 1. kord koige alguses $i = 0
+	// 2. enne sisu iga kord $i < $age
+	// 3. p2rast sisu iga kord $i = $i+1
+	
+	for ($i = 1; $i <= $age; $i = $i + 1) {
+		
+		echo $i."palju ";
+		
+	}
+	
+	echo "õnne";
+	
+
+?>
