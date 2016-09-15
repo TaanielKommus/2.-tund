13cfb2b3616463fc6c166ff19126b7025c5ad938
taankomm <taankomm@greeny.cs.tlu.ee>
2016-09-15 15:06:30 +0300

    Login n signin

---
+++ login.php
@@ -0,0 +1,87 @@
+<?php
+
+	//var_dump($_GET);
+	
+	//echo "<br>";
+	
+	//var_dump($_POST);
+	
+	$signupEmailError = "*";
+	
+	// kas keegi vajutas nuppu ja see on olemas
+	
+	if (isset ($_POST["signupEmail"])) {
+		
+		//on olemas
+		// kas epost on tühi
+		
+		if (empty ($_POST["signupEmail"])) {
+			
+			// on tühi
+			$signupEmailError = "Väli on kohustuslik";
+			
+		}
+		
+	}
+	
+	$signupPasswordError = "*";
+	
+	
+	if (isset ($_POST["signupPassword"])) {
+		
+		
+		if (empty ($_POST["signupPassword"])) {
+			
+			$signupPasswordError = "Väli on kohustuslik";
+			
+		}
+		
+	}
+	
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sisselogimise leht</title>
+	</head>
+	<body>
+
+		<h1>Logi sisse</h1>
+		
+		<form method="POST" >
+			
+			<label> E-post</label><br>
+			<input name="loginEmail" type="email">
+			
+			<br><br>
+			<label> Parool</label><br>
+			<input name="loginPassword" type="password">
+			
+			<br><br>
+			
+			<input type="submit" value="Logi sisse">
+			
+		</form>
+		
+
+		<h1>Loo kasutaja</h1>
+		
+		<form method="POST" >
+			
+			<label> E-post</label><br>
+			<input name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+			
+			<br><br>
+			<label> Parool</label><br>
+			<input name="signupPassword" type="password"> <?php echo $signupPasswordError; ?>
+			
+			<br><br>
+			
+			<input type="submit" value="Loo kasutaja">
+			
+		</form>
+		
+	</body>
+</html>
